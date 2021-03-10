1cc9338b8ac432beeac9a290d928f003299470e3
Nguyễn Anh Tuấn <i-2021-nguyenanhtuan@fois-vn.com>
2021-03-10 10:35:05 +0700

    Project Settings

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Content from "./components/Content";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header */}
+      <Header />
+      {/* Main Content */}
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/css/Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="demoH1">This is Header</h1>
+    </div>
+  );
+};
+
+export default Header;
