28fe33759a2bf7d86d2d8321b9aaee2e7025a9a2
Nguyễn Anh Tuấn <i-2021-nguyenanhtuan@fois-vn.com>
2021-03-22 09:40:17 +0700

    Change styles for front end

--- Pinterest_FrontEnd/src/services/auth.service.js
+++ Pinterest_FrontEnd/src/services/auth.service.js
@@ -1,11 +1,11 @@
-import { requestUrl } from '../config/request-url';
-import { requestService } from './request.service';
+import { requestUrl } from "../config/request-url";
+import { requestService } from "./request.service";
 
-const login = payload => requestService.post(requestUrl.auth, payload);
+const login = (payload) => requestService.post(requestUrl.auth, payload);
 
-const register = payload => requestService.post(requestUrl.register, payload);
+const register = (payload) => requestService.post(requestUrl.register, payload);
 
 export const authService = {
-    login,
-    register
-}
+  login,
+  register,
+};
