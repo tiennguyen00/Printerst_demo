efdf07eb4c4a3b163285a81b3bb724780fc1acf9
tinhtrinh <chumeoconxauxa@gmail.com>
2021-04-19 21:02:56 +0700

    thêm hover css vào pin

--- Pinterest_FrontEnd/src/components/Pin/Pin.jsx
+++ Pinterest_FrontEnd/src/components/Pin/Pin.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
+import "./Pin.scss";
 
 const Wrapper = styled.div`
   display: inline-flex;
@@ -23,11 +27,24 @@ const Container = styled.div`
 
 const Pin = (props) => {
   let { urls } = props;
+  const [isLike, setIsLike] = useState(false);
+
+  const test = () => {
+    console.log('tét')
+  }
+  
+  const save = () => {
+    console.log('save');
+  };
 
   return (
     <Wrapper>
-      <Container>
+      <Container className="pin-container">
         <img src={urls?.regular} alt="pin" />
+        <div className="pin-overlay" onClick={() => test()} ></div>
+        <button className="pin-save-btn" onClick={() => save()}>Save</button>
+        {isLike ? <div onClick={() => {setIsLike(!isLike)}} className="pin-like-btn"><FavoriteIcon  fontSize="large" style={{ color: '#e7e5e6' }}/></div>
+                : <div onClick={() => {setIsLike(!isLike)}} className="pin-like-btn"><FavoriteBorderIcon  fontSize="large" style={{ color: '#e7e5e6' }}/></div>}
       </Container>
     </Wrapper>
   );
