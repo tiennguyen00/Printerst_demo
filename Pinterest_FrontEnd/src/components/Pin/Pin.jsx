f9456213c076b5a68129d1823129a743b13c4570
tinhtrinh <chumeoconxauxa@gmail.com>
2021-04-19 08:36:17 +0700

    sửa css pin

--- Pinterest_FrontEnd/src/components/Pin/Pin.jsx
+++ Pinterest_FrontEnd/src/components/Pin/Pin.jsx
@@ -1,36 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
-  display: inline-flex;
-  padding: 0 1 0 2 px;
-`;
-
-const Container = styled.div`
-  display: inline-flex;
-  align-items: center;
-  box-sizing: border-box;
-  cursor: pointer;
-  width: 243px;
-
-  img {
-    display: flex;
-    width: 100%;
-    cursor: zoom-in;
-    border-radius: 19px;
-    object-fit: cover;
-  }
-`;
+import './Pin.scss';
 
 const Pin = (props) => {
   let { urls } = props;
 
-  return (
-    <Wrapper>
-      <Container>
-        <img src={urls?.regular} alt="pin" />
-      </Container>
-    </Wrapper>
+  return ( 
+      <div className="wrapper">
+        <div className="container">
+          <img src={urls?.regular} alt="pin" />
+        </div>
+      </div>
   );
 };
 
