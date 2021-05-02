74174d8173aab35159ce5903dec57edcca7594d5
18521487@gm.uit.edu.vn <18521487@gm.uit.edu.vn>
2021-05-02 22:52:43 +0700

    Lấy commit 'sửa lại post của Tuấn' của nhánh tinh

--- Pinterest_FrontEnd/src/components/Post/Post.jsx
+++ Pinterest_FrontEnd/src/components/Post/Post.jsx
@@ -1,21 +1,18 @@
-import React, {useState, useEffect} from "react";
-import {useForm} from "react-hook-form";
-import {get} from 'lodash/get';
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { IconButton } from "@material-ui/core";
-import CancelIcon from '@material-ui/icons/Cancel';
+import CancelIcon from "@material-ui/icons/Cancel";
 
-import { user } from '../../util/user';
-import { getMess } from '../../util/message';
-import { userService } from '../../services/user.service';
+import { user } from "../../util/user";
+import { userService } from "../../services/user.service";
 
-import "./Post.scss"
+import "./Post.scss";
+import { ContentContainer, FormWrapper, ImgWrapper } from "./styled-components";
 
-const Post = ({isPostOpen, closePost}) => {
-
-  const [file, setFile] = useState('');
-  const [file1, setFile1] = useState('');
-  const [imagePreviewUrl, setImg] = useState('');
-  const [userID, setUserID] = useState('');
+const Post = ({ isPostOpen, closePost }) => {
+  const [file, setFile] = useState("");
+  const [imagePreviewUrl, setImg] = useState("");
+  const [userID, setUserID] = useState("");
   const { register, handleSubmit } = useForm();
 
   const handleImageChange = (e) => {
@@ -26,60 +23,175 @@ const Post = ({isPostOpen, closePost}) => {
 
     reader.onloadend = () => {
       setFile(file);
-      setImg(reader.result)
+      setImg(reader.result);
+    };
+
+    setFile(e.target.files[0])
+    reader.readAsDataURL(file);
+  };
+
+  useEffect(() => {
+    async function fetchUserInfo() {
+      let userInfo = user.getUserInfo();
+      setUserID(userInfo.id);
     }
-
-    setFile1(e.target.files[0])
-    reader.readAsDataURL(file)
-  }
-
-  useEffect(async () => {
-    let userInfo = user.getUserInfo();
-  setUserID(userInfo.id);
-  console.log(userID)
+    fetchUserInfo();
   }, []);
 
-  
-
   const onSubmit = (data) => {
-    // setApiError('');
+    const { status } = data;
+    console.log(data)
 
-    const { status, file } = data;
+    let formData = new FormData();
+    formData.append("userID", userID);
+    formData.append("status", status);
+    formData.append("linkFile", file);
 
-    let formData  =  new FormData();
-    formData.append('userID', userID);
-    formData.append('status', status);
-    formData.append('linkFile', file1);
-  
-    console.log(formData);
     userService.post(formData);
-    // authService.updateRegisterProfile(formData)
-    //     .then(() => history.push(stateHistory.prePath || '/home'))
-    //     .catch((err) => setApiError(err.message));
-}
+  };
 
-  let $imagePreview = imagePreviewUrl ? (<span className="imgPreview"><img src={imagePreviewUrl}/></span>): '';
+  let $imagePreview = imagePreviewUrl ? (
+    <div
+      className="imgPreview"
+      style={{ border: "1px solid black", borderRadius: "20px", position: "relative" }}
+    >
+      <img
+        src={imagePreviewUrl}
+        alt="Preview"
+        style={{ borderRadius: "20px", position: "absolute", left: 0, objectFit: "cover"}}
+      />
+      <input
+        id="file-input"
+        type="file"
+        accept="image/bmp,image/gif,image/jpeg,image/png,image/tiff,image/webp"
+        aria-hidden="true"
+        style={{
+          cursor: "pointer",
+          width: "100%",
+          height: "100%",
+          opacity: "0",
+          zIndex: '999',
+          position: 'absolute',
+          left: 0
+        }}
+        name="file"
+        ref={register}
+        onChange={(e) => handleImageChange(e)}
+      />
+    </div>
+  ) : (
+    ""
+  );
+
+  let $showInput = imagePreviewUrl ? (
+    ""
+  ) : (
+    <div
+      className="input"
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        border: "1px solid black",
+        borderRadius: "20px",
+      }}
+    >
+      <p style={{ position: "absolute", textAlign: "center", width: "100%" }}>
+        Drag and drop or click here to upload{" "}
+      </p>
+
+      <input
+        id="file-input"
+        type="file"
+        accept="image/bmp,image/gif,image/jpeg,image/png,image/tiff,image/webp"
+        aria-hidden="true"
+        style={{
+          cursor: "pointer",
+          width: "100%",
+          height: "100%",
+          opacity: "0",
+        }}
+        name="file"
+        ref={register}
+        onChange={(e) => handleImageChange(e)}
+      />
+    </div>
+  );
 
   return isPostOpen ? (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <section>
-        <div className="text">
-          <h1 className="post-title">Create Post</h1>
-          <span className="close" onClick={closePost}>
-            <IconButton>
-             <CancelIcon fontSize="large"/>
-            </IconButton>
-          </span>
-          <hr/>
-          <img src="http://placehold.it/100/100"/>
-          <textarea name="status" ref={register} placeholder="What's in your mind"></textarea>
-          <input className="file-input" type="file" ref={register} name="file" onChange={(e)=> handleImageChange(e)}/>
-          {$imagePreview}
-          <input type="submit" value="post"/>
+    <div className="overlay">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className="wrapper">
+          <div className="postImg-form">
+            <div className="title" style={{ width: "100%", height: "30px" }}>
+              <h1 className="post-title">Post Your Image</h1>
+            </div>
+
+            <span className="close" onClick={closePost}>
+              <IconButton>
+                <CancelIcon fontSize="large" />
+              </IconButton>
+            </span>
+
+            <ContentContainer>
+              <ImgWrapper>
+              {$showInput} {$imagePreview}
+              </ImgWrapper>
+
+              <FormWrapper className="form">
+                <input
+                  type="text"
+                  placeholder="Create a title"
+                  style={{
+                    border: "none",
+                    height: "40px",
+                    fontSize: "24px",
+                    outline: "none",
+                    width: "100%",
+                    paddingLeft: "10px",
+                  }}
+                />
+                <hr
+                  style={{
+                    opacity: "0.7",
+                    width: " 100%",
+                    margin: "0 auto",
+                  }}
+                />
+
+                <textarea
+                  name="status"
+                  ref={register}
+                  placeholder="What's in your mind?"
+                />
+
+                <hr
+                  style={{
+                    opacity: "0.7",
+                    width: " 100%",
+                    margin: "0 auto",
+                  }}
+                />
+
+                <input className="submit-button" type="submit" value="post" />
+              </FormWrapper>
+            </ContentContainer>
+          </div>
         </div>
-      </section>
-      <div class="overlay"></div>
-    </form>
+      </form>
+    </div>
   ) : (
     ""
   );
